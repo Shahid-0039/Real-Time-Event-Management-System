4701422eae10c85133fac9e581f8b7d52d6682b8
Tahir Mahmood <tahirarorai@gmail.com>
2024-12-17 23:04:10 +0500

    Update Participant.java

--- EventManagemnetSystemGUI/EventManagemnetSystemGUI/EventManagemnetSystemGUI/src/eventmanagemnetsystemgui/Participant.java
+++ EventManagemnetSystemGUI/EventManagemnetSystemGUI/EventManagemnetSystemGUI/src/eventmanagemnetsystemgui/Participant.java
@@ -1,34 +1,32 @@
 package eventmanagemnetsystemgui;
-
-public class Participant {
+public class Participant{
     private int participantID;
     private String participantName;
     private boolean isRegistered;
-
-    public Participant(int participantID, String participantName) {
+    public Participant(int participantID, String participantName){
         this.participantID = participantID;
         this.participantName = participantName;
-        this.isRegistered = false; // Initially, the participant is not registered
+        this.isRegistered = false; 
     }
 
-    public int getParticipantID() {
+    public int getParticipantID(){
         return participantID;
     }
 
-    public String getParticipantName() {
+    public String getParticipantName(){
         return participantName;
     }
 
-    public boolean isRegistered() {
+    public boolean isRegistered(){
         return isRegistered;
     }
 
-    public void setRegistered(boolean registered) {
+    public void setRegistered(boolean registered){
         isRegistered = registered;
     }
 
     @Override
-    public String toString() {
+    public String toString(){
         return "Participant ID: " + participantID + ", Name: " + participantName + ", Registered: " + isRegistered;
     }
 }
